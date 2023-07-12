81fa4ac067bdea4937c58a6d3433963d5dc72600
Pratichhya <39898768+Pratichhya@users.noreply.github.com>
2023-07-12 14:11:03 +0200

    Additional added

--- Data/Datafunctions/DataAvailabilty.py
+++ Data/Datafunctions/DataAvailabilty.py
@@ -10,7 +10,8 @@ def main(c):
         "Landsat-5": ComplementaryOffer,
         "Landsat-7": ComplementaryOffer,
         "Landsat-8": ComplementaryOffer,
-        "Commercial data": VHROffer
+        "Commercial data": VHROffer,
+        "AdditionalComplementaryData": Additional,
     }
     AvailabilityTable = cases.get(constellation, general)(c)
     return AvailabilityTable
@@ -187,6 +188,66 @@ def VHROffer(c):
             t.append([Provider, Satellite, Product_type, Resolution, Access_type])
             note += footnotes
             headers = ["Dataset provider ","Satellite constellation", "Product Type  ", "Spatial Resolution ", "Type of Access"]
+
+        # Find empty columns
+        for j in range(len(t[0])):
+            column_values = [row[j] for row in t]
+            if all(value == '' for value in column_values):
+                empty_columns.append(j)
+
+        # Remove empty columns
+        
+        headers = [header for i, header in enumerate(headers) if i not in empty_columns]
+        t = [[row[i] for i in range(len(headers))] for row in t]
+
+        table = tabulate(t, headers=headers, tablefmt='html', floatfmt=".4f", stralign="center", numalign="center")
+        # Set the minimum width of each column to 100 pixels
+        table = table.replace("<table>", '<table class="table">')
+        table = f"""<h5>{tabletitle}</h5>{table}{note}"""
+    except:
+        table = " "
+    
+    return table
+
+
+def Additional(c):
+    tabletitle = "Offered Data"
+    
+    try: 
+        data_offer = len(c['summaries']['DataAvailability'])
+        t = []
+        note = ""
+        empty_columns = []  # Track empty columns
+
+        for i in range(0, data_offer):
+            try:
+                Product_type = c['summaries']['DataAvailability'][i]['Product_type']
+            except:
+                Product_type = ''
+            try:
+                SpecificProduct = c['summaries']['DataAvailability'][i]['SpecificProduct']
+            except:
+                SpecificProduct = ''
+            try:
+                Spatial = c['summaries']['DataAvailability'][i]['Spatial']
+            except:
+                Spatial = ''
+            try:
+                Temporal = c['summaries']['DataAvailability'][i]['Temporal']
+            except:
+                Temporal = ''
+            try:
+                ProductLink = c['summaries']['DataAvailability'][i]['ProductLink']
+            except:
+                ProductLink = ''
+            try:
+                footnotes = c['summaries']['DataAvailability'][i]['Note']
+            except:
+                footnotes = ''
+
+            t.append([Product_type, SpecificProduct, Spatial, Temporal, ProductLink])
+            note += footnotes
+            headers = ["Product Type", "Specific Products", "Spatial Extext","Temporal Extent", "Product Detail"]
 
         # Find empty columns
         for j in range(len(t[0])):
