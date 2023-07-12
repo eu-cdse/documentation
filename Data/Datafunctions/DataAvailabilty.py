814ca305335edb46a2f57c86df2ea5db06b517f5
Pratichhya <39898768+Pratichhya@users.noreply.github.com>
2023-07-12 13:53:44 +0200

    VHR Sample table added

--- Data/Datafunctions/DataAvailabilty.py
+++ Data/Datafunctions/DataAvailabilty.py
@@ -9,7 +9,8 @@ def main(c):
         "MERIS": ComplementaryOffer,
         "Landsat-5": ComplementaryOffer,
         "Landsat-7": ComplementaryOffer,
-        "Landsat-8": ComplementaryOffer
+        "Landsat-8": ComplementaryOffer,
+        "Commercial data": VHROffer
     }
     AvailabilityTable = cases.get(constellation, general)(c)
     return AvailabilityTable
@@ -148,3 +149,61 @@ def ComplementaryOffer(c):
     return table
 
 
+def VHROffer(c):
+    tabletitle = "Offered Data"
+    
+    try: 
+        data_offer = len(c['summaries']['DataAvailability'])
+        t = []
+        note = ""
+        empty_columns = []  # Track empty columns
+
+        for i in range(0, data_offer):
+            try:
+                Provider = c['summaries']['DataAvailability'][i]['Provider']
+            except:
+                Provider = ''
+            try:
+                Satellite = c['summaries']['DataAvailability'][i]['Satellite']
+            except:
+                Satellite = ''
+            try:
+                Product_type = c['summaries']['DataAvailability'][i]['Product_type']
+            except:
+                Product_type = ''
+            try:
+                Resolution = c['summaries']['DataAvailability'][i]['Resolution']
+            except:
+                Resolution = ''
+            try:
+                Access_type = c['summaries']['DataAvailability'][i]['Access_type']
+            except:
+                Access_type = ''
+            try:
+                footnotes = c['summaries']['DataAvailability'][i]['Note']
+            except:
+                footnotes = ''
+
+            t.append([Provider, Satellite, Product_type, Resolution, Access_type])
+            note += footnotes
+            headers = ["Dataset provider ","Satellite constellation", "Product Type  ", "Spatial Resolution ", "Type of Access"]
+
+        # Find empty columns
+        for j in range(len(t[0])):
+            column_values = [row[j] for row in t]
+            if all(value == '' for value in column_values):
+                empty_columns.append(j)
+
+        # Remove empty columns
+        
+        headers = [header for i, header in enumerate(headers) if i not in empty_columns]
+        t = [[row[i] for i in range(len(headers))] for row in t]
+
+        table = tabulate(t, headers=headers, tablefmt='html', floatfmt=".4f", stralign="center", numalign="center")
+        # Set the minimum width of each column to 100 pixels
+        table = table.replace("<table>", '<table class="table">')
+        table = f"""<h5>{tabletitle}</h5>{table}{note}"""
+    except:
+        table = " "
+    
+    return table
